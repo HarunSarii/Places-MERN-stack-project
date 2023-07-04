4ec365c93c203f6fc251c3d15829400cacb6eaaa
HarunSarii <harunsari14@gmail.com>
2023-07-04 11:17:31 +0300

    feat: add input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import './NewPlace.css'
+import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
-    <form className='place-form' >
-      
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
     </form>
-  )
-}
+  );
+};
 
-export default NewPlace
+export default NewPlace;
