ce2a3a276789d1d5b85128a05f00ba9da6ad38a0
HarunSarii <harunsari14@gmail.com>
2023-07-10 14:33:44 +0300

    feat: add address input and form submit

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,15 +1,15 @@
-import React, { useCallback, useReducer } from "react";
-import Input from "../../shared/components/FormElements/Input";
-import "./NewPlace.css";
-import Button from "../../shared/components/FormElements/Button";
+import React, { useCallback, useReducer } from 'react';
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+import Button from '../../shared/components/FormElements/Button';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
-} from "../../shared/util/validators";
+} from '../../shared/util/validators';
 
 const formReducer = (state, action) => {
   switch (action.type) {
-    case "INPUT_CHANGE":
+    case 'INPUT_CHANGE':
       let formIsValid = true;
       for (const inputId in state.inputs) {
         if (inputId === action.inputId) {
@@ -20,42 +20,47 @@ const formReducer = (state, action) => {
       }
       return {
         ...state,
-        input: {
+        inputs: {
           ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid },
+          [action.inputId]: { value: action.value, isValid: action.isValid }
         },
-        isValid: formIsValid,
+        isValid: formIsValid
       };
     default:
       return state;
   }
 };
 
-const NewPlace = (state, action) => {
+const NewPlace = () => {
   const [formState, dispatch] = useReducer(formReducer, {
     inputs: {
       title: {
-        value: "",
-        isValid: false,
+        value: '',
+        isValid: false
       },
       description: {
-        value: "",
-        isValid: false,
-      },
+        value: '',
+        isValid: false
+      }
     },
-    isValid: true,
+    isValid: false
   });
   const inputHandler = useCallback((id, value, isValid) => {
     dispatch({
-      type: "INPUT_CHANGE",
+      type: 'INPUT_CHANGE',
       value: value,
       isValid: isValid,
-      inputId: id,
+      inputId: id
     });
-  }, [dispatch]);
+  }, []);
+
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs); // send this to the backend!
+  };
 
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -69,11 +74,21 @@ const NewPlace = (state, action) => {
         id="description"
         element="textarea"
         label="Description"
-        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
         onInput={inputHandler}
       />
-      <Button type='submit' disabled={!formState.isValid} >ADD PLACE</Button>
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   );
 };
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,53 +1,56 @@
-import React, { useReducer, useEffect } from "react";
-import {validate} from '../../util/validators'
-import "./Input.css";
+import React, { useReducer, useEffect } from 'react';
+import './Input.css';
+import { validate } from '../../util/validators';
 
 const inputReducer = (state, action) => {
   switch (action.type) {
-    case "CHANGE":
+    case 'CHANGE':
       return {
         ...state,
         value: action.val,
-        isValid: validate(action.val, action.validators),
+        isValid: validate(action.val, action.validators)
       };
-      case 'TOUCH':
-        return {
-          ...state,
-          isTouched: true
-        }
+    case 'TOUCH': {
+      return {
+        ...state,
+        isTouched: true
+      }
+    }
     default:
       return state;
   }
 };
 
-const Input = (props) => {
+const Input = props => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: "",
-    isValid: false,
-    isTouched: false
+    value: '',
+    isTouched: false,
+    isValid: false
   });
 
-  const {id, onInput} = props;
-  const {value, isValid} = inputState;
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
 
   useEffect(() => {
-    props.onInput(props.id, inputState.value, inputState.isValid)
-  },[props, inputState])
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput]);
 
-  const changeHandler = (event) => {
+  const changeHandler = event => {
     dispatch({
-      type: "CHANGE",
+      type: 'CHANGE',
       val: event.target.value,
-      validators: props.validators,
+      validators: props.validators
     });
   };
-const touchHandler = () => {
-dispatch({
-  type: 'TOUCH'
-})
-}
+
+  const touchHandler = () => {
+    dispatch({
+      type: 'TOUCH'
+    });
+  };
+
   const element =
-    props.element === "input" ? (
+    props.element === 'input' ? (
       <input
         id={props.id}
         type={props.type}
@@ -65,11 +68,11 @@ dispatch({
         value={inputState.value}
       />
     );
+
   return (
     <div
-      className={`form-control ${
-        !inputState.isValid && inputState.isTouched && "form-control--invalid"
-      }`}
+      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+        'form-control--invalid'}`}
     >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
