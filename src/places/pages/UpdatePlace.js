356a8d4787c80848427c229a09b86db55ff0fa66
HarunSarii <harunsari14@gmail.com>
2023-07-11 11:15:13 +0300

    feat: adjust form-hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
-import './PlaceForm.css';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import "./PlaceForm.css";
 import { useForm } from "../../shared/hooks/form-hook";
 
 const DUMMY_PLACES = [
@@ -22,7 +25,7 @@ const DUMMY_PLACES = [
   },
   {
     id: "p2",
-    title: "Emp. State Building",
+    title: "Empire State Building",
     description: "One of the most famous sky scrapers in the world!",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
@@ -36,20 +39,46 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
-  const { placeId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const placeId = useParams().placeId;
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
-  const [formState, inputHandler] = useForm({
-    title: {
-      value: identifiedPlace.title,
-      isValid: true
-    },
-    description: {
-      value: identifiedPlace.description,
-      isValid: true
-    }
-  }, true)
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true,
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true,
+        },
+      },
+      true
+    );
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace]);
 
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
 
   if (!identifiedPlace) {
     return (
@@ -59,20 +88,23 @@ const UpdatePlace = () => {
     );
   }
 
-  const placeUpdateSubmitHandler  = (event) => {
-    event.preventDefault();
-    console.log('inputs', formState.inputs);
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );
   }
 
   return (
-    <form className="place-form" onSubmit={placeUpdateSubmitHandler} >
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
         id="title"
         element="input"
         type="text"
-        label="title"
+        label="Title"
         validators={[VALIDATOR_REQUIRE()]}
-        errorText='Please enter a valid title.'
+        errorText="Please enter a valid title."
         onInput={inputHandler}
         initialValue={formState.inputs.title.value}
         initialValid={formState.inputs.title.isValid}
@@ -82,13 +114,14 @@ const UpdatePlace = () => {
         element="textarea"
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText='Please enter a valid description (at least 5 characters).'
+        errorText="Please enter a valid description (min. 5 characters)."
         onInput={inputHandler}
         initialValue={formState.inputs.description.value}
         initialValid={formState.inputs.description.isValid}
       />
-      <Button type='submit' disabled={!formState.isValid} >UPDATE PLACE</Button>
-
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
     </form>
   );
 };
