8906702bb69c6c48c32eb0342f7d839fa49f4296
HarunSarii <harunsari14@gmail.com>
2023-07-29 22:46:20 +0300

    fix: fix api typo

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -11,7 +11,8 @@ const Users = () => {
     const sendRequest = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch("http:localhost:5000/api/users");
+        const response = await fetch("http://localhost:5000/api/users");
+
         const responseData = await response.json();
         if (!response.ok) {
           throw new Error(responseData.message);
